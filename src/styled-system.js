88d6d3ac071a77bb3e1682988dfb7b8c3b2f3d95
Aleksey <aleksey.amelchenko@gmail.com>
2020-03-26 12:57:53 +0300

    Add styled-system

---
+++ src/styled-system.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import {
+  space,
+  color,
+  background,
+  flexbox,
+  layout,
+  border,
+  fontSize,
+  textAlign
+} from "styled-system";
+
+export const Flex = styled.div`
+  display: flex;
+  ${space}
+  ${color}
+  ${background}
+  ${flexbox}
+  ${layout}
+  ${border}
+  ${fontSize}
+  ${textAlign}
+`;
