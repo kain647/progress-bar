7d6632e21655d7c3c3c395728fe269671e5f1374
Aleksey <aleksey.amelchenko@gmail.com>
2020-03-26 12:59:53 +0300

    Initial commit

---
+++ src/globals.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #eee;
+  }
+`;
